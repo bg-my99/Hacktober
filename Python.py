be8f39738193d894de4167cf63c34bed547bc218
bg-my99 <92119067+bg-my99@users.noreply.github.com>
2022-10-19 09:43:08 +0100

    Add files via upload

---
+++ Python.py
@@ -0,0 +1,7 @@
+username = input("Enter username: ")
+# print("Username is " + username)
+password = input("Enter password: ")
+if password=="Password":
+    print("Welcome Ben")
+else:
+    print("Access Denied")
